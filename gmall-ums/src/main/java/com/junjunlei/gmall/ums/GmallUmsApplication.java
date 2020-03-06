75267b5181610525bc013d8bd2e0e0f4ef506ed8
雷俊俊 <junjun.lei@hand-china.com>
2020-03-06 15:39:35 +0800

    [FIX]:修改springboot版本为2.1.6

--- gmall-ums/src/main/java/com/junjunlei/gmall/ums/GmallUmsApplication.java
+++ gmall-ums/src/main/java/com/junjunlei/gmall/ums/GmallUmsApplication.java
@@ -3,6 +3,9 @@ package com.junjunlei.gmall.ums;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Junjunlei
+ */
 @SpringBootApplication
 public class GmallUmsApplication {
 
