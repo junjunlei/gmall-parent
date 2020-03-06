807a634a879b59d5de85c2ba9a036d3c771fed9b
雷俊俊 <junjun.lei@hand-china.com>
2020-03-06 15:30:04 +0800

    [init]:初始化项目

---
+++ gmall-admin-web/src/main/java/com/junjunlei/gmall/GmallAdminWebApplication.java
@@ -0,0 +1,13 @@
+package com.junjunlei.gmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallAdminWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallAdminWebApplication.class, args);
+    }
+
+}
---
+++ gmall-oms/src/main/java/com/junjunlei/gmall/oms/GmallOmsApplication.java
@@ -0,0 +1,16 @@
+package com.junjunlei.gmall.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Junjunlei
+ */
+@SpringBootApplication
+public class GmallOmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallOmsApplication.class, args);
+    }
+
+}
