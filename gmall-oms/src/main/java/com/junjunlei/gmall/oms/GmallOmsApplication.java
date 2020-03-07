cdc9bfd884f1a8ea6367b0c829f1243e8e261723
雷俊俊 <junjun.lei@hand-china.com>
2020-03-07 11:04:20 +0800

    [ADD]:OMS系统配置Dubbo、Mybatis、并测试调通

--- gmall-oms/src/main/java/com/junjunlei/gmall/oms/GmallOmsApplication.java
+++ gmall-oms/src/main/java/com/junjunlei/gmall/oms/GmallOmsApplication.java
@@ -1,11 +1,15 @@
 package com.junjunlei.gmall.oms;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author Junjunlei
  */
+@EnableDubbo
+@MapperScan(basePackages = "com.junjunlei.gmall.oms.mapper")
 @SpringBootApplication
 public class GmallOmsApplication {
 
--- gmall-oms/src/test/java/com/junjunlei/gmall/oms/GmallOmsApplicationTests.java
+++ gmall-oms/src/test/java/com/junjunlei/gmall/oms/GmallOmsApplicationTests.java
@@ -1,7 +1,10 @@
 package com.junjunlei.gmall.oms;
 
+import com.junjunlei.gmall.oms.entity.Order;
+import com.junjunlei.gmall.oms.service.OrderService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class GmallOmsApplicationTests {
 
+    @Autowired
+    private OrderService orderService;
     @Test
     public void contextLoads() {
+        Order order = orderService.getById(26);
+        System.out.println(order.getNote());
     }
 
 }
