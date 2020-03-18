9ba9ee234de23c2f15aa173b1fe0f8a9e1f7ad78
雷俊俊 <junjun.lei@hand-china.com>
2020-03-19 00:50:32 +0800

    [ADD]:后台管理系统搭建

--- gmall-admin-web/src/main/java/com/junjunlei/gmall/GmallAdminWebApplication.java
+++ gmall-admin-web/src/main/java/com/junjunlei/gmall/GmallAdminWebApplication.java
@@ -1,9 +1,18 @@
 package com.junjunlei.gmall;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+/**
+ * @author junjun.lei
+ * @since 2020-03-07
+ */
+@EnableDubbo
+@EnableSwagger2
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GmallAdminWebApplication {
 
     public static void main(String[] args) {
